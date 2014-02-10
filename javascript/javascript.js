811790045bcc6308aa8bf1f86de94a6e31509d59
Damien Rosa <damien.guedes.r@gmail.com>
2014-02-10 19:27:08 +0000

    improve transitions between menus

---
+++ javascript/javascript.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('#logo').click(function(){
+		$.scrollTo('#home', 1000, {offset: {top:-63}});
+	});
+	$('#ab').click(function(){
+		$.scrollTo('#about_me', 1000, {offset: {top:-63}});
+	});
+	$('#pro').click(function(){
+		$.scrollTo('#profile', 1000, {offset: {top:-63}});
+	});
+	$('#edu').click(function(){
+		$.scrollTo('#education', 1000, {offset: {top:-63}});
+	});
+	$('#edu').click(function(){
+		$.scrollTo('#education', 1000, {offset: {top:-63}});
+	});
+	$('#ski').click(function(){
+		$.scrollTo('#skills', 1000, {offset: {top:-63}});
+	});
+	$('#con').click(function(){
+		$.scrollTo('#contact', 1000, {offset: {top:-63}});
+	});
+});
