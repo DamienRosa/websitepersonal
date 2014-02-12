e0dafacd25c4d4debc7bdc0943f9fcae5c177bd3
Damien Rosa <damien.guedes.r@gmail.com>
2014-02-12 00:09:50 +0000

    Finished transition between menus, finished about me and improve profile and home

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,41 @@
 $(document).ready(function(){
-	$('#logo').click(function(){
-		$.scrollTo('#home', 1000, {offset: {top:-63}});
-	});
-	$('#ab').click(function(){
-		$.scrollTo('#about_me', 1000, {offset: {top:-63}});
-	});
-	$('#pro').click(function(){
-		$.scrollTo('#profile', 1000, {offset: {top:-63}});
-	});
-	$('#edu').click(function(){
-		$.scrollTo('#education', 1000, {offset: {top:-63}});
-	});
-	$('#edu').click(function(){
-		$.scrollTo('#education', 1000, {offset: {top:-63}});
-	});
-	$('#ski').click(function(){
-		$.scrollTo('#skills', 1000, {offset: {top:-63}});
-	});
-	$('#con').click(function(){
-		$.scrollTo('#contact', 1000, {offset: {top:-63}});
-	});
+
+
+	/*========= easeoutQuad function ==========*/
+	$.easing.easeoutQuad = 	function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	};
+
+
+	/*========= function Full Page ==========*/
+	function fullPageSection(){
+		var vecSection = document.querySelectorAll('section>section');
+		for (var i = vecSection.length - 1; i >= 0; i--) {
+			$(vecSection[i]).height($(window).height()-159);
+		};
+	}
+
+	/*========= function Menu ==========*/
+	function menu(){
+		scrollAll('#logo', '#home');
+		var vecMenu = document.getElementById('menu_top').getElementsByTagName('li');
+		var vecSection = document.querySelectorAll('section>section');
+		for (var i = 0; i < vecMenu.length; ++i) {
+			scrollAll('#'+vecMenu[i].id, '#'+vecSection[i+1].id);
+		}
+	};
+
+	/*========= Run Functions ==========*/
+	menu();
+	fullPageSection();
 });
+
+
+/*========= function Scroll ==========*/
+function scrollAll(idMenu, idSection){
+	$(idMenu).click(function(evt){
+		$.scrollTo(idSection, {duration:800, easing:'easeoutQuad', offset: {top:-57} });
+		evt.preventDefault();
+	});
+}
